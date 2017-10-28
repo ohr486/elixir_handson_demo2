2c80f4c72b65a7b6a80be2aa78f3daedb541cbd7
ohr486 <ohr486@gmail.com>
2017-10-28 19:04:11 +0900

    add comment

--- lib/demo_web/controllers/hello_controller.ex
+++ lib/demo_web/controllers/hello_controller.ex
@@ -1,8 +1,10 @@
 defmodule DemoWeb.HelloController do
-  use DemoWeb, :controller
+  use DemoWeb, :controller # コントローラー関連の機能を使うための指定
 
+  # conn : コネクションを表現する構造体、render関数はconnを受け取ってVIEWを描画する
+  # params : URLパラメータなどの情報を持つマップ
   def hello(conn, params) do
-    name = params["name"]
-    render(conn, "hello.html", %{name: name})
+    name = params["name"] # nameというキーをparamsマップから取り出す
+    render(conn, "hello.html", %{who: name}) # hello.htmlというテンプレートを、whoというキーに名前を入れて描画する
   end
 end
